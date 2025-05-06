a0ef2b0252f5be4b2a9af7f5d8e036b16701e5d7
unknown <rudacate02@hotmail.com>
2025-05-05 23:55:08 -0600

    ultimos cambios

--- TheModel.py
+++ TheModel.py
@@ -1,20 +1,30 @@
 import tensorflow as tf
 
-class build:
+class Build:
     @staticmethod
     def build_it():
-        model = tf.keras.Sequential([
-            tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
-            tf.keras.layers.MaxPooling2D((2, 2)),
-            tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-            tf.keras.layers.MaxPooling2D((2, 2)),
-            tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(64, activation='relu'),
-            tf.keras.layers.Dense(10, activation='softmax')
-        ])
+        inputs = tf.keras.layers.Input(shape=(28, 28, 1))
+
+        # simple normalisation
+        x = tf.keras.layers.Rescaling(1.0 / 255)(inputs)
+
+        # Conv Block 1
+        x = tf.keras.layers.Conv2D(32, 3, activation="relu", padding="same")(x)
+        x = tf.keras.layers.MaxPooling2D()(x)
+
+        # Conv Block 2
+        x = tf.keras.layers.Conv2D(64, 3, activation="relu", padding="same")(x)
+        x = tf.keras.layers.MaxPooling2D()(x)
+
+        # Head
+        x = tf.keras.layers.Flatten()(x)
+        x = tf.keras.layers.Dense(128, activation="relu")(x)
+        outputs = tf.keras.layers.Dense(10, activation="softmax")(x)
+
+        model = tf.keras.Model(inputs, outputs)
         model.compile(
-            optimizer='adam',
-            loss='sparse_categorical_crossentropy',
-            metrics=['accuracy']
+            optimizer="adam",
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+            metrics=["accuracy"],
         )
         return model
